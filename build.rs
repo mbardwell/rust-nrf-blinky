d95954d099272b94398efbe4cd9f614e629e5cc6
Mike Bardwell <michael.bardwell@wyvern.space>
2023-09-15 11:09:22 -0400

    also pass custom flags to linker for examples

--- build.rs
+++ build.rs
@@ -32,4 +32,8 @@ fn main() {
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+
+    println!("cargo:rustc-link-arg-examples=--nmagic");
+    println!("cargo:rustc-link-arg-examples=-Tlink.x");
+    println!("cargo:rustc-link-arg-examples=-Tdefmt.x");
 }
